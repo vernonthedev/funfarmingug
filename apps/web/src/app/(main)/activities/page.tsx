583ef1e2c130bf6c9ede9d8cdf17b159500ba255
vernonthedev <techjaja2@gmail.com>
2026-07-30 18:24:45 +0300

    refactor(web): migrate activities page to Orgaanic service.html template

--- apps/web/src/app/(main)/activities/page.tsx
+++ apps/web/src/app/(main)/activities/page.tsx
@@ -42,54 +42,30 @@ export default function ActivitiesPage() {
                 ]}
             />
 
-            <div className="vl-service7">
-                <div className="vl-service7-area sp1">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-xl-6 mx-auto text-center">
-                                <div className="service7-header">
-                                    <h3 data-aos="fade-right" data-aos-duration="900">
-                                        <img src="/assets/img/icon/hm7-sub-title2-dot.png" alt="" />
-                                        Fun Activities
-                                    </h3>
-                                    <div className="space24"></div>
-                                    <h2 className="text-effect" data-aos="fade-left" data-aos-duration="1000">
-                                        Farm Games & Team Building For You & Your Herd
-                                    </h2>
-                                </div>
-                            </div>
-                            <div className="space44"></div>
-                            <div className="row">
-                                {activities.map((activity, index) => (
-                                    <div className="col-xl-3 col-lg-4 col-md-6" key={index}>
-                                        <div className="vl-services2-box" data-aos="fade-up" data-aos-duration="1000">
-                                            <div className="services2-box-icon">
-                                                <i className={activity.icon}></i>
-                                            </div>
-                                            <div className="space24"></div>
-                                            <div className="services2-box-content">
-                                                <h2>
-                                                    <Link href={activity.href}>{activity.title}</Link>
-                                                </h2>
-                                                <div className="space16"></div>
-                                                <p>{activity.text}</p>
-                                            </div>
-                                            <div className="services2-box-btn">
-                                                <Link href={activity.href} className="vl-btn4">
-                                                    Learn More
-                                                    <span className="arrow_btn4">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                                                            <path d="M27.002 16.002H5.00195" stroke="#132C12" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                                            <path d="M21.0021 22.002C21.0021 22.002 27.002 17.5831 27.002 16.002C27.002 14.4208 21.002 10.002 21.002 10.002" stroke="#132C12" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                                        </svg>
-                                                    </span>
-                                                </Link>
-                                            </div>
+            <div className="service-inr-box-area sp1">
+                <div className="container">
+                    <div className="row">
+                        {activities.map((activity, index) => (
+                            <div className="col-xl-3 col-lg-4 col-md-6" key={index}>
+                                <div className="service6-box margin-b30">
+                                    <div className="service6-logos">
+                                        <h3 className="title">
+                                            <a href={activity.href}>{activity.title}</a>
+                                        </h3>
+                                        <div className="inons">
+                                            <i className={activity.icon}></i>
                                         </div>
                                     </div>
-                                ))}
+                                    <div className="space24"></div>
+                                    <p className="pera-text">{activity.text}</p>
+                                    <div className="space28"></div>
+                                    <div className="service6-box-bottom">
+                                        <Link href={activity.href} className="btn3-home6">Learn More</Link>
+                                        <div className="step-number">0{index + 1}</div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
