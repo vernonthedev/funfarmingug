fff3f756ac1df34a97842ba296fe4ae043dd09f9
vernonthedev <techjaja2@gmail.com>
2026-07-30 15:13:12 +0300

    chore(infra): update pnpm-lock.yaml with new dependencies and versions for checkback package; add checkback to workspace configuration

--- checkback/app/page.tsx
+++ checkback/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <FontSwitcher>
             <Typewriter
-                text="Hey, thanks for stopping by @funfarminguganda! We are currently working on something new, please check back later :)"
+                text="Hey, thanks for stopping by @funfarminguganda! We are currently working on something new, please check back later =)"
                 delay={1200}
                 speed={50}
             />
