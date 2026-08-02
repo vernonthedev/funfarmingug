4dadd1745f3f2b29feff3f9efa659f2682906fbd
vernonthedev <techjaja2@gmail.com>
2026-08-02 14:14:44 +0300

    feat(web): port home page to DonalFarm theme with video hero

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,38 @@
+import Link from 'next/link';
+import {
+    Hero,
+    Benefits,
+    AboutUs,
+    Services,
+    QualityOfLife,
+    WhatWeDo,
+    OurAgriculture,
+    Projects,
+    Testimonials,
+    HappyFarm,
+    BlogPosts,
+    MeetFarmer,
+    ContactUs,
+    Partners,
+} from '@/components/home';
+
 export default function HomePage() {
-    return <div>Home</div>;
+    return (
+        <>
+            <Hero />
+            <Benefits />
+            <AboutUs />
+            <Services />
+            <QualityOfLife />
+            <WhatWeDo />
+            <OurAgriculture />
+            <Projects />
+            <Testimonials />
+            <HappyFarm />
+            <BlogPosts />
+            <MeetFarmer />
+            <ContactUs />
+            <Partners />
+        </>
+    );
 }
